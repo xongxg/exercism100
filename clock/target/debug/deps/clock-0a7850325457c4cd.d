90295f0d39b8d39f227062f74c9890ba0d6358fd
xongxg <xongxg@gmail.com>
2025-03-18 22:22:53 -0400

    clock implementation and test

---
+++ clock/target/debug/deps/clock-0a7850325457c4cd.d
@@ -0,0 +1,5 @@
+/Users/xongxg/works/rust/exercism100/clock/target/debug/deps/clock-0a7850325457c4cd: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/clock/target/debug/deps/clock-0a7850325457c4cd.d: src/lib.rs
+
+src/lib.rs:
---
+++ clock/target/debug/deps/clock-f0c45adfbb07ce1e.d
@@ -0,0 +1,7 @@
+/Users/xongxg/works/rust/exercism100/clock/target/debug/deps/libclock-f0c45adfbb07ce1e.rmeta: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/clock/target/debug/deps/libclock-f0c45adfbb07ce1e.rlib: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/clock/target/debug/deps/clock-f0c45adfbb07ce1e.d: src/lib.rs
+
+src/lib.rs:
