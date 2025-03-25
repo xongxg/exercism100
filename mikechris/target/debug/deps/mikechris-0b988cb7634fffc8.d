e243a3062b01f492f01f41d0fc94ea878582c3e1
xongxg <xongxg@gmail.com>
2025-03-24 20:55:04 -0400

    bottle-song difference-of-squares leap luhn prime-factors sublist sum-of-multiples

---
+++ mikechris/target/debug/deps/mikechris-0b988cb7634fffc8.d
@@ -0,0 +1,7 @@
+/Users/xongxg/works/rust/exercism100/mikechris/target/debug/deps/libmikechris-0b988cb7634fffc8.rmeta: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/mikechris/target/debug/deps/libmikechris-0b988cb7634fffc8.rlib: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/mikechris/target/debug/deps/mikechris-0b988cb7634fffc8.d: src/lib.rs
+
+src/lib.rs:
---
+++ mikechris/target/debug/deps/mikechris-6148e90e75e513b6.d
@@ -0,0 +1,5 @@
+/Users/xongxg/works/rust/exercism100/mikechris/target/debug/deps/mikechris-6148e90e75e513b6: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/mikechris/target/debug/deps/mikechris-6148e90e75e513b6.d: src/lib.rs
+
+src/lib.rs:
