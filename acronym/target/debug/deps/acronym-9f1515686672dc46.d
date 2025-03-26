024892e177b37be4e8e3778373d1a8d88f3b3682
xongxg <xongxg@gmail.com>
2025-03-26 00:55:29 -0400

    acronym

---
+++ acronym/target/debug/deps/acronym-9f1515686672dc46.d
@@ -0,0 +1,5 @@
+/Users/xongxg/works/rust/exercism100/acronym/target/debug/deps/acronym-9f1515686672dc46: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/acronym/target/debug/deps/acronym-9f1515686672dc46.d: src/lib.rs
+
+src/lib.rs:
---
+++ acronym/target/debug/deps/acronym-f88e814bec998bca.d
@@ -0,0 +1,7 @@
+/Users/xongxg/works/rust/exercism100/acronym/target/debug/deps/libacronym-f88e814bec998bca.rmeta: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/acronym/target/debug/deps/libacronym-f88e814bec998bca.rlib: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/acronym/target/debug/deps/acronym-f88e814bec998bca.d: src/lib.rs
+
+src/lib.rs:
