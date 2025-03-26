54769c9ac8869bdbdc4737953eb9be03963a2390
xongxg <xongxg@gmail.com>
2025-03-25 21:46:47 -0400

    kindergarten

---
+++ kindergarten-garden/target/debug/deps/kindergarten_garden-f17510b264128ceb.d
@@ -0,0 +1,5 @@
+/Users/xongxg/works/rust/exercism100/kindergarten-garden/target/debug/deps/kindergarten_garden-f17510b264128ceb: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/kindergarten-garden/target/debug/deps/kindergarten_garden-f17510b264128ceb.d: src/lib.rs
+
+src/lib.rs:
