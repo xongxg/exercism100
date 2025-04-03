917371776fdee3d02abacc7946c55733411a0731
xongxg <xongxg@gmail.com>
2025-04-02 22:03:52 -0400

    pangram

---
+++ pangram/target/debug/deps/pangram-57411957a48e52da.d
@@ -0,0 +1,5 @@
+/Users/xongxg/works/rust/exercism100/pangram/target/debug/deps/pangram-57411957a48e52da: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/pangram/target/debug/deps/pangram-57411957a48e52da.d: src/lib.rs
+
+src/lib.rs:
---
+++ pangram/target/debug/deps/pangram-d0a36f9c028a16f6.d
@@ -0,0 +1,7 @@
+/Users/xongxg/works/rust/exercism100/pangram/target/debug/deps/libpangram-d0a36f9c028a16f6.rmeta: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/pangram/target/debug/deps/libpangram-d0a36f9c028a16f6.rlib: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/pangram/target/debug/deps/pangram-d0a36f9c028a16f6.d: src/lib.rs
+
+src/lib.rs:
