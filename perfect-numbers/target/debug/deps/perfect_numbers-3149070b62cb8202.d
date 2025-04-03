049a44d6283a8982fa7d75e52f4ff810fdbf5f7a
xongxg <xongxg@gmail.com>
2025-04-03 10:34:28 -0400

    perfect-numbers

---
+++ perfect-numbers/target/debug/deps/perfect_numbers-3149070b62cb8202.d
@@ -0,0 +1,7 @@
+/Users/xongxg/works/rust/exercism100/perfect-numbers/target/debug/deps/libperfect_numbers-3149070b62cb8202.rmeta: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/perfect-numbers/target/debug/deps/libperfect_numbers-3149070b62cb8202.rlib: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/perfect-numbers/target/debug/deps/perfect_numbers-3149070b62cb8202.d: src/lib.rs
+
+src/lib.rs:
---
+++ perfect-numbers/target/debug/deps/perfect_numbers-4f4a7a3267b68c15.d
@@ -0,0 +1,5 @@
+/Users/xongxg/works/rust/exercism100/perfect-numbers/target/debug/deps/perfect_numbers-4f4a7a3267b68c15: src/lib.rs
+
+/Users/xongxg/works/rust/exercism100/perfect-numbers/target/debug/deps/perfect_numbers-4f4a7a3267b68c15.d: src/lib.rs
+
+src/lib.rs:
